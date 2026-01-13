8ef2225edd89b506e8665cfd50f4a35162ea01d7
Nikhil Gholakar <nikhil.gholkar76@gmail.com>
2026-01-13 13:55:54 -0800

    bug fixes

--- public/script.js
+++ public/script.js
@@ -33,8 +33,8 @@ document.getElementById("jobForm").addEventListener("submit", async (e) => {
   // 🔹 IMAGE GENERATION
   const canvas = await html2canvas(card, {
     scale: 2,
-    width: 800,
-    height: 800
+    width: 1080,
+    height: 1080
   });
 
   // 🔹 DOWNLOAD BUTTON
